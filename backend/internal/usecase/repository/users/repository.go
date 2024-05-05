3644ac7c9f4b0fc30191873cf7c0b9e84a2f4c64
optclblast <molodoy.p4elovek@mail.ru>
2024-05-05 20:50:55 +0300

    tmp

--- backend/internal/usecase/repository/users/repository.go
+++ backend/internal/usecase/repository/users/repository.go
@@ -2,13 +2,15 @@ package users
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/emochka2007/block-accounting/internal/pkg/models"
+	"github.com/google/uuid"
 )
 
 type GetParams struct {
-	Id             string
-	OrganizationId string
+	Ids            uuid.UUIDs
+	OrganizationId uuid.UUIDs
 	Seed           []byte
 }
 
@@ -19,4 +21,32 @@ type Repository interface {
 	Activate(ctx context.Context, id string) error
 	Update(ctx context.Context, user *models.User) error
 	Delete(ctx context.Context, id string) error
+}
+
+type repositorySQL struct {
+	db *sql.DB
+}
+
+func NewRepository(db *sql.DB) Repository {
+	return nil
+}
+
+func (r *repositorySQL) Get(ctx context.Context, params GetParams) (*models.User, error) {
+
+}
+
+func (r *repositorySQL) Create(ctx context.Context, user *models.User) error {
+
+}
+
+func (r *repositorySQL) Activate(ctx context.Context, id string) error {
+
+}
+
+func (r *repositorySQL) Update(ctx context.Context, user *models.User) error {
+
+}
+
+func (r *repositorySQL) Delete(ctx context.Context, id string) error {
+
 }
